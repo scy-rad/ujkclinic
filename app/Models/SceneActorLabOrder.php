f76f617808aa5ad11086b5104c3126c1fc654f5c
Penknife SIM <gitsim@scyzoryk.info>
2023-03-13 11:08:15 +0100

    check privillages in uodate order form (Scene Actor Lab Order)

--- app/Models/SceneActorLabOrder.php
+++ app/Models/SceneActorLabOrder.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class SceneActorLabOrder extends Model
